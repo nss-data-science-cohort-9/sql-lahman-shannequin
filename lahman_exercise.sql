fb20a0d6c7ab6dc45d323915eb1f347da4f60d65
shannequin <shannequin@gmail.com>
2026-03-17 11:02:43 -0500

    Complete Q10

--- lahman_exercise.sql
+++ lahman_exercise.sql
@@ -560,10 +560,30 @@ ORDER BY PLAYER_NAME, HITS;
 -- 10. Find all players who hit their career highest number of home runs in 2016. 
 -- Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. 
 -- Report the players' first and last names and the number of home runs they hit in 2016.
-SELECT *
+WITH
+	PLAYERS_TEN_YR AS (
+		SELECT
+			PLAYERID,
+			COUNT(YEARID) AS YEARS
+		FROM BATTING
+		WHERE YEARID <= 2016
+		GROUP BY PLAYERID
+		HAVING COUNT(YEARID) >= 10
+	)
+SELECT
+	PL.NAMEFIRST || ' ' || PL.NAMELAST AS PLAYER_NAME,
+	B.YEARID,
+	MAX(B.HR) AS MAX_HR
 FROM BATTING B
-	INNER JOIN PEOPLE P USING (PLAYERID)
-LIMIT 5;
+	INNER JOIN PLAYERS_TEN_YR P USING (PLAYERID)
+	INNER JOIN PEOPLE PL USING (PLAYERID)
+WHERE B.YEARID <= 2016
+	AND B.HR > 0
+GROUP BY P.PLAYERID, PLAYER_NAME, B.YEARID
+HAVING B.YEARID = 2016
+ORDER BY MAX_HR DESC;
+
+-- ANSWER: (115 ROWS)
 
 -- After finishing the above questions, here are some open-ended questions to consider.
 
